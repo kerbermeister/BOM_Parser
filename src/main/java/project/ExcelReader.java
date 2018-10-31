01ef24b308eeba0b8b78e85bd97736811ff06ac3
techtonix <flatleywalker@yandex.ru>
2018-10-31 15:31:54 +0300

    deleted some imports

--- src/main/java/project/ExcelReader.java
+++ src/main/java/project/ExcelReader.java
@@ -1,21 +1,13 @@
 package project;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class ExcelReader {
     private Workbook workbook;
-
-    public ExcelReader() {
-
-    }
 
     public ExcelReader(Workbook workbook) throws IOException {
         this.workbook = workbook;
@@ -24,7 +16,6 @@ public class ExcelReader {
     public int getNumberOfSheets() {
         return workbook.getNumberOfSheets();
     }
-
 
     public Iterator<Row> getExcelList(int sheetIndex) {
         return workbook.getSheetAt(sheetIndex).rowIterator();
--- src/main/java/project/PartsPatterns/PatternsToIgnore.java
+++ src/main/java/project/PartsPatterns/PatternsToIgnore.java
@@ -21,7 +21,5 @@ public class PatternsToIgnore {
         ignoreList.add("audio");
         ignoreList.add("pad");
         ignoreList.add("antivibration");
-
-
     }
 }
